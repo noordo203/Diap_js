d3af13590dc621e46fee5ac3b5da53704215b957
noordo203 <alex.maerten@free.fr>
2014-11-04 20:25:49 +0100

    Ajout de la config' javascript

---
+++ diap_javascript_config.js
@@ -0,0 +1,27 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: "Ce que j'ai retenu du<br />JAVASCRIPT",
+    subtitle: '... des leçons de Codecademy.com',
+    useBuilds: true,        // Default: true. False will turn off slide animation builds.
+    usePrettify: true,      // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true,      // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'images/ico.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Alexandre M.',
+    company: 'TS1 - Lycée NOORDOVER',
+//  gplus: 'http://plus.google.com/1234567890',
+//  twitter: '@yourhandle',
+    www: 'http://noordover.org'
+//  github: 'http://github.com/you'
+  }]
+};
